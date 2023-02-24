7e54ed58deb5e9503ac45b16d3045c106b141871
Daniele La Corte <danielelc44@gmail.com>
2023-02-24 11:21:05 +0100

    init commit

--- zzapr/Student.java
+++ zzapr/Student.java
@@ -2,6 +2,6 @@ package zzapr;
 
 public class Student {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Daniele La Corte University of Palermo Mechanical engineering");
     }
 }
