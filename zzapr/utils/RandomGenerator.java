c28314ce18c3d187c9b5237c437aea527acbef26
Daniele La Corte <danielelc44@gmail.com>
2023-03-10 11:44:36 +0100

    task03

---
+++ zzapr/utils/RandomGenerator.java
@@ -0,0 +1,11 @@
+package zzapr.utils;
+
+public class RandomGenerator {
+    //TODO Create a method that will generate random number from defined interval
+    // interval <a, b>
+
+    public static int getRandomNumber(int a, int b) {
+        return (int) (a + (b - a) * Math.random());
+    }
+}
+
---
+++ zzapr/week03.java
@@ -0,0 +1,14 @@
+package zzapr;
+import zzapr.utils.Week03;
+public class week03 {
+
+    public static void main(String[] args){
+        int[] arr = new int[10];
+        Week03.generateRandArray(arr);
+        Week03.writeRandArray(arr);
+        Week03.sortAscending(arr);
+        Week03.writeRandArray(arr);
+        Week03.arrayOppositeDirection(arr);
+        Week03.writeRandArray(arr);
+    }
+}
